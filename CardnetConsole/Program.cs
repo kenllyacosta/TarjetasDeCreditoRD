acad6ecdcad519b1ce73f96c8199bf6b8d02a98e
Kenlly Acosta <kenllyacosta@hotmail.com>
2023-07-24 13:01:16 -0400

    Agregado método para extraer las ip's del equipo local. GetLocalIPAdressV4();

--- CardnetConsole/Program.cs
+++ CardnetConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using ECRti.Framework;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
 
 namespace CarnetConsole
 {
@@ -6,13 +9,13 @@ namespace CarnetConsole
     {
         static void Main(string[] args)
         {
-            string ipLocal = "192.168.1.100", ipRemota = "192.168.1.200", portNumberLocal = "2018", portNumberRemote = "7060";
-            int timeOut = 30790;
+            string ipLocal = "192.168.8.10", portNumberLocal = "2018", ipRemota = "192.168.8.150", portNumberRemote = "7060";                   
+            int timeOut = 180000;
             Core core = new();
 
-            Console.WriteLine(core.SetTimeout(timeOut));
             Console.WriteLine(core.SetLocalEndPoint(ipLocal, int.Parse(portNumberLocal)));
             Console.WriteLine(core.SetRemoteEndPoint(ipRemota, int.Parse(portNumberRemote)));
+            Console.WriteLine(core.SetTimeout(timeOut));
 
             Console.WriteLine(core.Initialice());
 
@@ -20,6 +23,18 @@ namespace CarnetConsole
 
             Console.WriteLine("Hello, World!");
             Console.ReadLine();
+        }
+
+        private static List<string> GetLocalIPAdressV4()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            var ipAddressV4 = new List<string>();
+
+            foreach (var ip in ipHostInfo.AddressList)
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    ipAddressV4.Add(ip.ToString());
+
+            return ipAddressV4;
         }
     }
 
